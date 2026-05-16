ca72360157371acb0f1099c228202951a03ddfdd
Gim2510 <guglielmino2510@gmail.com>
2026-05-16 20:43:46 +0200

    TBDO-33/34: PostHog integration + consent-aware tracking hook
    - AnalyticsProvider wraps app, reads gdprConsent
    - PostHog SDK initialized only when analytics: true
    - useAnalytics hook exposes trackEvent, trackPageview
    - updateAnalyticsConsent syncs banner toggle with tracking
    - Vercel + PostHog both respect consent state

--- src/App.tsx
+++ src/App.tsx
@@ -30,15 +30,17 @@ import {TermsOfService} from "./Pages/TermOfService.tsx";
 import {CookiePolicy} from "./Pages/CookiePolicy.tsx";
 import {About} from "./Pages/About.tsx";
 import { Analytics } from "@vercel/analytics/react";
+import { AnalyticsProvider } from "./providers/AnalyticsProvider.tsx";
 
 function App() {
     return (
         <div className='bg-[#111110]'>
             <ThemeProvider>
-                <AuthProvider>
-                    <PremiumProvider>
-                        <ScrollProvider />
-                        <Routes>
+                <AnalyticsProvider>
+                    <AuthProvider>
+                        <PremiumProvider>
+                            <ScrollProvider />
+                            <Routes>
                             <Route path="/" element={<LayoutHomepage><Home/></LayoutHomepage>}></Route>
                             <Route path="/survey/start" element={<LayoutHomepage><SurveyStart /></LayoutHomepage>} />
                             <Route path="/login" element={<LayoutHomepage><Login /></LayoutHomepage>} />
@@ -63,6 +65,7 @@ function App() {
                         </Routes>
                     </PremiumProvider>
                 </AuthProvider>
+                </AnalyticsProvider>
             </ThemeProvider>
             <Analytics />
         </div>
